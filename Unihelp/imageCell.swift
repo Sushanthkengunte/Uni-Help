9312cc897d64bad0b41cb788aaf81282f6fdbe70
pampas93 <abhijit.93@hotmail.com>
2017-04-15 19:19:32 -0400

    Finsihed House adding form

---
+++ Unihelp/imageCell.swift
@@ -0,0 +1,28 @@
+//
+//  imageCell.swift
+//  Unihelp
+//
+//  Created by Abhijit Srikanth on 4/15/17.
+//  Copyright © 2017 SuProject. All rights reserved.
+//
+
+import UIKit
+
+class imageCell: UITableViewCell {
+    
+    @IBOutlet weak var imageout: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    
+    
+}
